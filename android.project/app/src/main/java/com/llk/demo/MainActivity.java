0669671a7903eb521d77e619978511854b727b4a
yudan <505747044@qq.com>
2016-04-28 23:02:02 +0800

    navigation bar hide.

--- android.project/app/src/main/java/com/llk/demo/MainActivity.java
+++ android.project/app/src/main/java/com/llk/demo/MainActivity.java
@@ -1,13 +1,121 @@
 package com.llk.demo;
 
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bHide, bDisplay;
+
+    private static Handler sHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init() {
+        bHide = (Button) findViewById(R.id.btn_Hide);
+        bDisplay = (Button) findViewById(R.id.btn_Display);
+
+        bHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleHideyBar();
+            }
+        });
+
+        bDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                sHandler = new Handler();
+
+                sHandler.post(mHideRunnable); // hide the navigation bar
+
+                final View decorView = getWindow().getDecorView();
+                decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        sHandler.post(mHideRunnable); // hide the navigation bar
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * Detects and toggles immersive mode (also known as "hidey bar" mode).
+     */
+    public void toggleHideyBar() {
+
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        boolean isImmersiveModeEnabled =
+                ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+            Log.i("llk", "Turning immersive mode mode off. ");
+        } else {
+            Log.i("llk", "Turning immersive mode mode on.");
+        }
+
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.  For the purposes of this sample
+        // all three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+    }
+
+
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            int flags;
+            int curApiVersion = android.os.Build.VERSION.SDK_INT;
+            // This work only for android 4.4+
+            if(curApiVersion >= Build.VERSION_CODES.KITKAT){
+                // This work only for android 4.4+
+                // hide navigation bar permanently in android activity
+                // touch the screen, the navigation bar will not show
+                flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+            }else{
+                // touch the screen, the navigation bar will show
+                flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            }
+
+            // must be executed in main thread :)
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+        }
+    };
 }
